f2cca4543b983c1d5b577a8f4fc5474c1c6d77f3
Amina <ka_bouzar@esi.dz>
2022-05-14 21:57:54 +0100

    partie jeu joueur de

---
+++ De.java
@@ -0,0 +1,17 @@
+import  java.util.concurrent.ThreadLocalRandom;
+/**
+ * Write a description of class De here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class De
+{
+
+    public static int lancerDe()
+    {
+        
+        // put your code here
+        return ThreadLocalRandom.current().nextInt(1,7);
+    }
+}
